1fe5a54c1b8a4a0922849b337123165809a04598
andrea-ds-lab <andrea.deltasystem@gmail.com>
2024-09-10 11:55:01 +0200

    added multi-channel support

--- lib/socket_test_web/channels/testing_channel_channel.ex
+++ lib/socket_test_web/channels/testing_channel_channel.ex
@@ -2,7 +2,7 @@ defmodule SocketTestWeb.TestingChannelChannel do
   use SocketTestWeb, :channel
 
   @impl true
-  def join("testing_channel:lobby", payload, socket) do
+  def join("testing_channel:" <> _channel_name, payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -10,29 +10,33 @@ defmodule SocketTestWeb.TestingChannelChannel do
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
   @impl true
   def handle_in("ping", payload, socket) do
     {:reply, {:ok, payload}, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (testing_channel:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
 
+  # Channels can be used in a request/response fashion by sending replies to requests from the client
+  @impl true
   def handle_in("btn_track", payload, socket) do
     IO.inspect(payload)
     {:noreply, socket}
   end
 
-  def handle_in(_, payload, socket) do
-    #IO.puts("messaggio ricevuto")
+  # It is also common to receive messages from the client and broadcast to everyone in the current topic (testing_channel:_channel_name).
+  @impl true
+  def handle_in("new_msg", payload, socket) do
     broadcast(socket, "new_msg", payload)
+    {:noreply, socket}
+  end
+
+  def handle_in(_, payload, socket) do
+    # Handle other messages if needed
     {:noreply, socket}
   end
 

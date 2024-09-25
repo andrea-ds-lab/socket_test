95118bba518575e8977373c714d2980cedb216fa
andrea-ds-lab <andrea.deltasystem@gmail.com>
2024-09-25 11:58:05 +0200

    update messages APIs

--- lib/socket_test/chat/message.ex
+++ lib/socket_test/chat/message.ex
@@ -1,6 +1,9 @@
 defmodule SocketTest.Chat.Message do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+
+  @max_amount_of_messages_to_fetch 100
 
   @derive {Jason.Encoder,
            only: [:id, :user, :body, :boosted, :channel, :inserted_at, :updated_at]}
@@ -19,4 +22,31 @@ defmodule SocketTest.Chat.Message do
     |> cast(attrs, [:body, :user, :boosted, :channel])
     |> validate_required([:body, :user, :channel])
   end
+
+  def fetch_latest_messages(repo, id \\ nil, amount) do
+    IO.puts("#{repo}, #{id}, #{amount}")
+
+    messages =
+      case id do
+        nil ->
+          # If id is nil, fetch the latest messages directly
+          from(m in __MODULE__,
+            order_by: [desc: m.id],
+            limit: ^min(amount, @max_amount_of_messages_to_fetch)
+          )
+          |> repo.all()
+
+        _ ->
+          # If id is provided, use the existing logic to filter messages
+          from(m in __MODULE__,
+            where: m.id < ^id,
+            order_by: [desc: m.id],
+            limit: ^min(amount, @max_amount_of_messages_to_fetch)
+          )
+          |> repo.all()
+      end
+
+    messages
+  end
+
 end
--- lib/socket_test_web/message_controller.ex
+++ lib/socket_test_web/message_controller.ex
@@ -13,7 +13,7 @@ defmodule SocketTestWeb.MessageController do
           |> where([m], m.inserted_at >= ^start_time)
           |> Repo.all()
 
-    json(conn, %{messages: messages})
+        json(conn, %{messages: messages})
 
       {:error, _} ->
         conn
@@ -22,10 +22,33 @@ defmodule SocketTestWeb.MessageController do
     end
   end
 
+  def index(conn, %{"amount" => amount} = params) do
+    amount = String.to_integer(amount)
+
+    case Map.get(params, "id") do
+      nil ->
+        # No "id" provided, fetch the latest messages (either 100 or fewer)
+        messages = Message.fetch_latest_messages(Repo, nil, amount)
+        json(conn, %{messages: messages})
+
+      id ->
+        # If "id" is provided, ensure it's a valid integer
+        case Integer.parse(id) do
+          {parsed_id, ""} ->
+            messages = Message.fetch_latest_messages(Repo, parsed_id, amount)
+            json(conn, %{messages: messages})
+
+          :error ->
+            conn
+            |> put_status(:bad_request)
+            |> json(%{error: "Invalid id format"})
+        end
+    end
+  end
+
   def index(conn, _params) do
     query =
       from m in Message,
-        # Change to `desc` if you want descending order
         order_by: [asc: m.inserted_at]
 
     messages = Repo.all(query)
